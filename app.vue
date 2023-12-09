a0c21ad3d3fa88eb21a00e60d8c4c0353124e754
AsyloSoft <asylosoft@outlook.com>
2023-12-09 09:52:38 -0600

    add navbar

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtPage />
+    <nav class="navbar bg-dark">
+      <div class="container-fluid">
+        <NuxtLink to="/" class="navbar-brand text-bg-dark">WebToolkit</NuxtLink>
+      </div>
+    </nav>
+    <div class="p-3">
+      <NuxtPage />
+    </div>
   </div>
 </template>
