97779c6101f1c8baa81f63fabaaa936f7a3ffc52
AsyloSoft <asylosoft@outlook.com>
2024-05-17 23:08:18 -0500

    move tools to single page

--- app/README.md
+++ app/README.md
@@ -1,28 +1,11 @@
-# Web Toolkit
+# Web Tool
+
+# Stack
+- NuxtJS (VueJS)
+- Bootstrap
 
 ## Host
-https://webtoolkit.asylosoft.com
+https://webtool.asylosoft.com
 
 ## Description
 A collection of simple tools available in the web.
-
-## Features
-- Client-side only
-- Built using Nuxt 3
-
-## Tools
-
-### [Converters](https://webtoolkit.asylosoft.com/converters)
-- [Letter case](https://webtoolkit.asylosoft.com/converter/lettercase)
-
-### [Encoders](https://webtoolkit.asylosoft.com/encoders)
-- [Base64](https://webtoolkit.asylosoft.com/encoder/base64)
-- [URL](https://webtoolkit.asylosoft.com/encoder/url)
-
-### [Generators](https://webtoolkit.asylosoft.com/generators)
-- [UUID](https://webtoolkit.asylosoft.com/generator/uuid)
-
-
-## Technology
-- NuxtJS (VueJS)
-- Bootstrap
--- app/app.vue
+++ app/app.vue
@@ -70,22 +70,10 @@ provide('toolkit', toolkit)
   <div class="d-flex flex-column vh-100">
     <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
       <div class="container-fluid">
-        <NuxtLink to="/" class="navbar-brand">WebToolkit</NuxtLink>
+        <NuxtLink to="/" class="navbar-brand">WebTool</NuxtLink>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item dropdown" v-for="category in toolkit">
-              <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{ category.title }}</a>
-              <ul class="dropdown-menu">
-                <li v-for="tool in category.tools">
-                  <NuxtLink :to="tool.url" class="dropdown-item">{{ tool.name }}</NuxtLink>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
       </div>
     </nav>
     <div class="bg-body-secondary p-3 flex-grow-1">
